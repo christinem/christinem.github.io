381e9a9d7d8afc2f936544cdb50265a426ef14c2
Christine Murad <christine.murad@mail.utoronto.ca>
2017-10-16 13:36:45 -0400

    Fixed nav bar issues and width of body

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
     });
      
     $(window).resize(function() {
-        if($( window ).width() == "600") {
+        if($( window ).width() >= "768") {
             $("header > nav").css("display", "block");
              
             if($("#logo").attr('class') == "menuDown") {
@@ -20,7 +20,7 @@ $(document).ready(function() {
     });
      
     $("header > nav > ul > li > a").click(function(e) {
-        if($( window ).width() == "600") { if($(this).siblings().size() == 0 ) {
+        if($( window ).width() >= "768") { if($(this).siblings().size() == 0 ) {
                 e.preventDefault();
                 $(this).siblings().slideToggle("fast")
                 $(this).children(".toggle").html($(this).children(".toggle").html() == 'close' ? 'expand' : 'close');
